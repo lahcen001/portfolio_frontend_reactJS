b2a0bb204d2ce4b3b6762bd083ed0518e6a2ccd9
lahcen001e <lahcen0000lh@gmail.com>
2022-04-26 10:23:55 +0000

    firt commit

---
+++ src/container/About/About.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import "./About.scss";
+import { motion } from "framer-motion";
+import { images } from "../../constants";
+import { urlFor, client } from "../../client";
+
+// const abouts = [
+//   {
+//     title: "Web development",
+//     description: "I am a good web developer",
+//     imgUrl: images.about01,
+//   },
+//   {
+//     title: "Web Design",
+//     description: "I am a good web developer",
+//     imgUrl: images.about02,
+//   },
+//   {
+//     title: "UI/UX",
+//     description: "I am a good web developer",
+//     imgUrl: images.about04,
+//   },
+//   {
+//     title: "Web Animations",
+//     description: "I am a good web developer",
+//     imgUrl: images.about04,
+//   },
+// ];
+function About() {
+  const [about, setAbout] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type=="abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbout(data);
+    });
+  }, []);
+  return (
+    <>
+      <h2 className="head-text">
+        I know that
+        <span> Good design </span>
+        <br />
+        means
+        <span> Good business</span>
+      </h2>
+      <div className="app__profiles">
+        {about.map((about, index) => (
+          <motion.div
+            whileInVie={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className="app__profiles-item"
+            key={about.title + index}
+          >
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default About;
---
+++ src/container/Header/Header.js
@@ -0,0 +1,77 @@
+import React from "react";
+import "./Header.scss";
+import { motion } from "framer-motion";
+import { images } from "../../constants";
+
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: "easeInOut",
+    },
+  },
+};
+
+function Header() {
+  return (
+    <div id="home" className="app__header app__flex">
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+      >
+        <div className="app__header-badge">
+          <div className="badge-cmp app__flex">
+            <span>👋</span>
+            <div style={{ marginLeft: 20 }}>
+              <p className="p-text">Hi, I'm</p>
+              <h1 className="head-text">Lahcen</h1>
+            </div>
+          </div>
+
+          <div className="tag-cmp app__flex">
+            <p className="p-text">web developer</p>
+            <p className="p-text"> Freelancer</p>
+          </div>
+        </div>
+      </motion.div>
+
+      {/* motion 2 */}
+
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__header-img"
+      >
+        <img src={images.avatar} alt="profile_bg" />
+
+        <motion.img
+          whileInView={{ scale: [0, 1] }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          className="overlay_circle"
+          alt="profile_circle"
+          src={images.circle}
+        />
+      </motion.div>
+
+      {/* motion 2 */}
+
+      <motion.div
+        variants={scaleVariants}
+        whileInView={scaleVariants.whileInView}
+        className="app__header-circles"
+      >
+        {[images.node, images.react, images.wordpress].map((images, index) => {
+          return (
+            <div className="circle-cmp app__flex " key={`${index}`}>
+              <img src={images} alt="circle" />
+            </div>
+          );
+        })}
+      </motion.div>
+    </div>
+  );
+}
+
+export default Header;
