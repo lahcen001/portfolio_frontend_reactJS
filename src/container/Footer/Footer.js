9b590a74918dcde8f6760bb0f9940310bb6ee00e
lahcen001e <lahcen0000lh@gmail.com>
2022-05-02 16:43:23 +0000

    V5

--- src/container/Footer/Footer.js
+++ src/container/Footer/Footer.js
@@ -1,11 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 import { images } from "../../constants";
 import { AppWrap, MotionWrap } from "../../wrapper";
 import { client } from "../../client";
 import "./Footer.scss";
-
+import emailjs from "@emailjs/browser";
 const Footer = () => {
+  const form = useRef();
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -16,28 +17,38 @@ const Footer = () => {
 
   const { username, email, message } = formData;
 
-  const handleChangeInput = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
+  // const handleChangeInput = (e) => {
+  //   const { name, value } = e.target;
+  //   setFormData({ ...formData, [name]: value });
+  // };
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
     setLoading(true);
 
-    const contact = {
-      _type: "contact",
-      name: formData.username,
-      email: formData.email,
-      message: formData.message,
-    };
+    e.preventDefault();
+    // console.log("e : ", e.target);
 
-    client
-      .create(contact)
-      .then(() => {
-        setLoading(false);
-        setIsFormSubmitted(true);
-      })
-      .catch((err) => console.log(err));
+    emailjs
+      .sendForm(
+        "service_gkr3cbq",
+        "template_8quf5se",
+        e.target,
+        "uZkU8oEQds4EJtOh4"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setLoading(false);
+          setIsFormSubmitted(true);
+
+          setTimeout(() => {
+            setIsFormSubmitted(false);
+          }, 1000 * 10);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
   };
 
   return (
@@ -59,15 +70,19 @@ const Footer = () => {
         </div>
       </div>
       {!isFormSubmitted ? (
-        <div className="app__footer-form app__flex">
+        <form
+          className="app__footer-form app__flex"
+          ref={form}
+          onSubmit={handleSubmit}
+        >
           <div className="app__flex">
             <input
               className="p-text"
               type="text"
               placeholder="Your Name"
               name="username"
-              value={username}
-              onChange={handleChangeInput}
+              // value={username}
+              // onChange={handleChangeInput}
             />
           </div>
           <div className="app__flex">
@@ -76,23 +91,25 @@ const Footer = () => {
               type="email"
               placeholder="Your Email"
               name="email"
-              value={email}
-              onChange={handleChangeInput}
+              // value={email}
+              // onChange={handleChangeInput}
             />
           </div>
           <div>
             <textarea
               className="p-text"
               placeholder="Your Message"
-              value={message}
+              // value={message}
               name="message"
-              onChange={handleChangeInput}
+              // onChange={handleChangeInput}
             />
           </div>
-          <button type="button" className="p-text" onClick={handleSubmit}>
-            {!loading ? "Send Message" : "Sending..."}
-          </button>
-        </div>
+          <input
+            type="submit"
+            className="p-text"
+            value={!loading ? "Send Message" : "Sending..."}
+          />{" "}
+        </form>
       ) : (
         <div>
           <h3 className="head-text">Thank you for getting in touch!</h3>
@@ -102,4 +119,4 @@ const Footer = () => {
   );
 };
 
-export default AppWrap(Footer, "app__footer");
+export default AppWrap(Footer, "footer");
